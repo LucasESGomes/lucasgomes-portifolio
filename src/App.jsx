e5c3fc14e47608b19780ae72a38d7358375efa79
Lucas Gomes <158865689+LucasESGomes@users.noreply.github.com>
2025-11-11 09:57:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//Components
+import Background from "./components/Background.jsx";
+import Header from "./components/Header.jsx";
+
+function App() {
+
+  return (
+    <>
+    <Background />
+    <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from '../assets/logo-preta-transparente.png'
+
+const Header = () => {
+    return (
+        <>
+            <Logo />
+        </>
+    )
+}
+
+export default Header;
