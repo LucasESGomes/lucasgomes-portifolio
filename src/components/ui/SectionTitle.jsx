b070186f507a7ed603c57739676a80f8d2afa9fa
LucasESGomes <lucasgomesferreira2008@gmail.com>
2025-11-19 23:19:54 -0300

    SectionTitle

---
+++ src/components/ui/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const NumberSection = ({number, title}) => {
+    return(
+        <div>
+            <h1 className="">{number}</h1>
+            <p>{title}</p>
+        </div>
+    );
+}
+
+export default NumberSection;
